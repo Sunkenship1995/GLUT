175777ab566e8bf2fa415d7a0f04d78a4178b992
sunkenship <moltke0316@gmail.com>
2017-07-05 00:17:52 +0900

    a

---
+++ Project1/Project1/prog1.cpp
@@ -0,0 +1,74 @@
+#include <gl\glut.h>
+
+GLdouble vertex[][3] = {
+	{ 0.0, 0.0, 0.0 },
+	{ 1.0, 0.0, 0.0 },
+	{ 1.0, 1.0, 0.0 },
+	{ 0.0, 1.0, 0.0 },
+	{ 0.0, 0.0, 1.0 },
+	{ 1.0, 0.0, 1.0 },
+	{ 1.0, 1.0, 1.0 },
+	{ 0.0, 1.0, 1.0 }
+};
+
+int edge[][2] = {
+	{ 0, 1 },
+	{ 1, 2 },
+	{ 2, 3 },
+	{ 3, 0 },
+	{ 4, 5 },
+	{ 5, 6 },
+	{ 6, 7 },
+	{ 7, 4 },
+	{ 0, 4 },
+	{ 1, 5 },
+	{ 2, 6 },
+	{ 3, 7 }
+};
+
+void display(void)
+{
+	int i;
+
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	/* ê}å`ÇÃï`âÊ */
+	glColor3d(0.0, 0.0, 0.0);
+	glBegin(GL_LINES);
+	for (i = 0; i < 12; ++i) {
+		glVertex3dv(vertex[edge[i][0]]);
+		glVertex3dv(vertex[edge[i][1]]);
+	}
+	glEnd();
+
+	glFlush();
+}
+
+void resize(int w, int h)
+{
+	glViewport(0, 0, w, h);
+
+	glLoadIdentity();
+	gluPerspective(30.0, (double)w / (double)h, 1.0, 100.0);
+	gluLookAt(3.0, 4.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+}
+
+
+
+
+void init(void)
+{
+	glClearColor(1.0, 1.0, 1.0, 1.0);
+}
+
+int main(int argc, char *argv[])
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGBA);
+	glutCreateWindow(argv[0]);
+	glutDisplayFunc(display);
+	glutReshapeFunc(resize);
+	init();
+	glutMainLoop();
+	return 0;
+}
---
+++ Project1/Project1/prog2.cpp
@@ -0,0 +1,142 @@
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <gl\glut.h>
+//
+//
+//#define MAXPOINTS 100      /* 記憶する点の数　　 */
+//GLint point[MAXPOINTS][2]; /* 座標を記憶する配列 */
+//int pointnum = 0;          /* 記憶した座標の数　 */
+//int rubberband = 0;        /* ラバーバンドの消去 */
+//
+//void display(void)
+//{
+//	int i;
+//
+//	glClear(GL_COLOR_BUFFER_BIT);
+//
+//	/* 記録したデータで線を描く */
+//	if (pointnum > 1) {
+//		glColor3d(0.0, 0.0, 0.0);
+//		glBegin(GL_LINES);
+//		for (i = 0; i < pointnum; ++i) {
+//			glVertex2iv(point[i]);
+//		}
+//		glEnd();
+//	}
+//	glFlush();
+//}
+//
+//void resize(int w, int h)
+//{
+//	/* ウィンドウ全体をビューポートにする */
+//	glViewport(0, 0, w, h);
+//
+//	/* 変換行列の初期化 */
+//	glLoadIdentity();
+//
+//	/* スクリーン上の座標系をマウスの座標系に一致させる */
+//	glOrtho(-0.5, (GLdouble)w - 0.5, (GLdouble)h - 0.5, -0.5, -1.0, 1.0);
+//}
+//
+//void mouse(int button, int state, int x, int y)
+//{
+//	/* 削除 */
+//
+//	switch (button) {
+//	case GLUT_LEFT_BUTTON:
+//		/* ボタンを操作した位置を記録する */
+//		point[pointnum][0] = x;
+//		point[pointnum][1] = y;
+//		if (state == GLUT_UP) {
+//			/* ボタンを押した位置から離した位置まで線を引く */
+//			glColor3d(0.0, 0.0, 0.0);
+//			glBegin(GL_LINES);
+//			glVertex2iv(point[pointnum - 1]); /* 一つ前は押した位置　 */
+//			glVertex2iv(point[pointnum]);     /* 今の位置は離した位置 */
+//			glEnd();
+//			glFlush();
+//
+//			/* 始点ではラバーバンドを描いていないので消さない */
+//			rubberband = 0;
+//		}
+//		else {
+//			/* 削除 */
+//		}
+//		if (pointnum < MAXPOINTS - 1) ++pointnum;
+//		break;
+//	case GLUT_MIDDLE_BUTTON:
+//		break;
+//	case GLUT_RIGHT_BUTTON:
+//		break;
+//	default:
+//		break;
+//	}
+//}
+//
+//void motion(int x, int y)
+//{
+//	static GLint savepoint[2]; /* 以前のラバーバンドの端点 */
+//
+//	/* 論理演算機能 ON */
+//	glEnable(GL_COLOR_LOGIC_OP);
+//	glLogicOp(GL_INVERT);
+//
+//	glBegin(GL_LINES);
+//	if (rubberband) {
+//		/* 以前のラバーバンドを消す */
+//		glVertex2iv(point[pointnum - 1]);
+//		glVertex2iv(savepoint);
+//	}
+//	/* 新しいラバーバンドを描く */
+//	glVertex2iv(point[pointnum - 1]);
+//	glVertex2i(x, y);
+//	glEnd();
+//
+//	glFlush();
+//
+//	/* 論理演算機能 OFF */
+//	glLogicOp(GL_COPY);
+//	glDisable(GL_COLOR_LOGIC_OP);
+//
+//	/* 今描いたラバーバンドの端点を保存 */
+//	savepoint[0] = x;
+//	savepoint[1] = y;
+//
+//	/* 今描いたラバーバンドは次のタイミングで消す */
+//	rubberband = 1;
+//}
+//
+//void keyboard(unsigned char key, int x, int y)
+//{
+//	switch (key) {
+//	case 'q':
+//	case 'Q':
+//	case '\033':  /* '\033' は ESC の ASCII コード */
+//		exit(0);
+//	default:
+//		break;
+//	}
+//}
+//
+//
+//void init(void)
+//{
+//	glClearColor(1.0, 0, 1.0, 1.0);
+//}
+//
+//int main(int argc, char *argv[])
+//{
+//	glutInitWindowPosition(100, 100);
+//	glutInitWindowSize(320, 240);
+//	glutInit(&argc, argv);
+//	glutInitDisplayMode(GLUT_RGBA);
+//	glutCreateWindow(argv[0]);
+//	glutDisplayFunc(display);
+//	glutReshapeFunc(resize);
+//	glutMotionFunc(motion);
+//	glutMouseFunc(mouse);
+//	glutKeyboardFunc(keyboard);
+//	init();
+//	glutMainLoop();
+//	return 0;
+//}
